0d34562f841d09034174760529989dd7875af05b
yankechao <yankechao@vipkid.com.cn>
2017-07-27 16:49:00 +0800

    使用vuex完成基础计数器

--- src/main.js
+++ src/main.js
@@ -6,7 +6,16 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         count: 0 
+    },
+    mutations: {
+        increment (state){
+            state.count++ 
+        }, 
+        reduce (state){
+            state.count-- 
+        }
     }
+
 });
 
 new Vue({
