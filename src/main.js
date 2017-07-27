f0884fa98113c676f6ec0979c66ad2f5b7376b85
yankechao <yankechao@vipkid.com.cn>
2017-07-27 16:26:33 +0800

    import vuex and init a simple store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {
+        count: 0 
+    }
+});
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
